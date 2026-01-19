67c13410d461aff46cfa5b00945802b32224ff07
cezicoder <cezarylachut1981@gmail.com>
2026-01-19 14:34:54 +0100

    rem text

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -96,9 +96,9 @@ export const Main = () => {
 
             <ModalTrigger className="bg-transparent  dark:bg-white dark:text-black text-black flex justify-center group/modal-btn">
 
-            <div className="flex justify-center">
+            <div className="flex  justify-center">
               <NoiseBackground
-                containerClassName="w-30 h-15 md:p-4 rounded-full mx-auto"
+                containerClassName="w-fit   md:p-4 rounded-full "
                 noiseIntensity={0.01}
                 speed={0.4}
                 backdropBlur={true}
@@ -109,8 +109,21 @@ export const Main = () => {
                   "rgb(255, 200, 100)",
                 ]}
               >
-                <button className="h-full w-full cursor-pointer rounded-full bg-linear-to-r from-neutral-100 via-neutral-100 to-white px-4 py-2 text-black shadow-[0px_2px_0px_0px_var(--color-neutral-50)_inset,0px_0.5px_1px_0px_var(--color-neutral-400)] transition-all duration-100 active:scale-98 dark:from-black dark:via-black dark:to-neutral-900 dark:text-white dark:shadow-[0px_1px_0px_0px_var(--color-neutral-950)_inset,0px_1px_0px_0px_var(--color-neutral-800)]">
-                  moje narzedzia &rarr;
+                <button className="
+                  h-full w-full cursor-pointer
+                  rounded-full
+                  px-3 py-1 text-sm
+                  sm:px-4 sm:py-2 sm:text-base
+                  bg-linear-to-r from-neutral-100 via-neutral-100 to-white
+                  text-black
+                  shadow-[0px_2px_0px_0px_var(--color-neutral-50)_inset,0px_0.5px_1px_0px_var(--color-neutral-400)]
+                  transition-all duration-100
+                  active:scale-98
+                  dark:from-black dark:via-black dark:to-neutral-900
+                  dark:text-white
+                  dark:shadow-[0px_1px_0px_0px_var(--color-neutral-950)_inset,0px_1px_0px_0px_var(--color-neutral-800)]
+                  ">
+                   narzedzia 
                 </button>
               </NoiseBackground>
             </div>
